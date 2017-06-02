82b8b8035e4d009f60144e7839d01d62f6c30a38
Huiben Zhang <n9074546@qut.edu.au>
2017-06-02 16:46:17 +1000

    d

--- NtripClient/NtripClient.cs
+++ NtripClient/NtripClient.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
 using System.Net.Sockets;
-using System.Net;
+using System.Net; 
 namespace Ntrip
 {
     public interface IClient
